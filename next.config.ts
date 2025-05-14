821fa279d4d4da4877d4054631c432acaa19fa63
Daevid The Great <mckinz147@gmail.com>
2025-05-14 03:20:05 +0100

    qwerty5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    // Custom Webpack configuration
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": "./src/components",
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
