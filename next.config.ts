d0cd07c9afb2b28c6c0d2b13e8150967f2be255e
Daevid The Great <mckinz147@gmail.com>
2025-05-14 17:58:16 +0100

    qwerty9

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,30 @@
 import PrismaWebpackPlugin from "prisma-webpack-plugin";
+import path from "path";
 import type { NextConfig } from "next";
 import type { Configuration } from "webpack";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   output: "standalone",
-  outputFileTracingIncludes: {
-      ".next/server/**/*": ["./prisma/generated/client/**/*"]
+    outputFileTracingIncludes: {
+      ".next/server/**/*.js": ["./prisma/generated/client/**/*"]
   },
   webpack(config: Configuration, { isServer }) {
     if (isServer) {
-      config.plugins = [...(config.plugins || []), new PrismaWebpackPlugin()];
+      config.plugins = [
+        ...(config.plugins || []),
+        new PrismaWebpackPlugin(),
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.join(process.cwd(), "prisma/generated/client"),
+              to: path.join(process.cwd(), ".next/server/prisma/generated/client")
+            }
+          ]
+        })
+      ];
     }
 
     return config;
